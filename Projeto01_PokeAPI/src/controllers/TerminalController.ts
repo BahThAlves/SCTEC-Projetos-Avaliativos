1c59b9d81354d4f70632db4cbbe2f20fb10ca4f0
Thiago <comsalmanteiga49@gmail.com>
2026-06-02 20:01:09 -0300

    terminalControllerRunning

--- Projeto01_PokeAPI/src/controllers/TerminalController.ts
+++ Projeto01_PokeAPI/src/controllers/TerminalController.ts
@@ -3,23 +3,25 @@ import { Interface } from 'node:readline';
 import { menuView } from '../view/viewers';
 
 export async function terminalController(interfaceConsole: Interface) {
-  menuView();
-  const opcao: string = await interfaceConsole.question('(1, 2, 3, 4): ');
+  let running = true;
 
-  switch (opcao) {
-    case '1':
-      break;
+  while (running) {
+    menuView();
+    const opcao: string = await interfaceConsole.question('(1, 2, 3, 4): ');
 
-    case '2':
-      break;
+    switch (opcao) {
+      case '1':
+        break;
 
-    case '3':
-      break;
+      case '2':
+        break;
 
-    case '4':
-      break;
+      case '3':
+        break;
 
-    default:
-      break;
+      case '4':
+        running = false;
+        break;
+    }
   }
 }
